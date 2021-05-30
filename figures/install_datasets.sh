eb403229748890cc8a701f026a7a2a792ca4c672
Benjamin Beresford-Jones <bsb31@cam.ac.uk>
2021-05-30 04:54:19 +0100

    Create install_datasets.sh

---
+++ figures/install_datasets.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Download large figure datasets that could not be hosted on GitHub.
+
+## Requirements:
+###  wget executable
+
+# Fig1_data
+wget https://zenodo.org/record/4855526/files/Fig1_data.zip
+# Fig4_data
+wget https://zenodo.org/record/4855526/files/Fig4_data.zip
+# SuppFig2_data
+wget https://zenodo.org/record/4855526/files/SuppFig2_data.zip
+# SuppFig4_data
+wget https://zenodo.org/record/4855526/files/SuppFig4_data.zip
+
+
+## unzip all datasets
+unzip *.zip
+rm -r __MACOSX/
